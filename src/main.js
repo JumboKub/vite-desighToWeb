cfbdf373f6306714b39e0a5ee61a13e91b46a009
Jumbo <pnilin1789@gmail.com>
2025-09-26 20:56:50 +0700

    ah i don't know, when i code i'm feel sooo much weird i think i should stop what i do. that it

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import './sass/main.scss'
+
+function toggleList() {
+    // Access the List
+    const courses = document.getElementById("tageMenu");
+
+    // Check the visibility
+    if (courses.style.display == "block") {
+        courses.style.display = "none"; // Hide the list
+    } else {
+        courses.style.display = "block"; // Display the list
+    }
+}
+
+// Hide the list on click outside the dropdown list
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown_button')) {
+        document.getElementById('courses_container')
+            .style.display = "none";
+    }
+}    
